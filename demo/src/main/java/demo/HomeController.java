f944ee17a1740b4a2a862e4921f03ff61206fddc
namho1802 <htnam02vvk@gmail.com>
2023-05-22 21:24:29 +0700

    initialize

---
+++ demo/src/main/java/demo/HomeController.java
@@ -0,0 +1,24 @@
+package demo;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet({"/trangchu"})
+public class HomeController extends HttpServlet {
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.doGet(req, resp);
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		req.getRequestDispatcher("/views/trangchu.jsp").forward(req, resp);
+	}
+}
